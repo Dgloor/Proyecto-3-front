3d91f0fc02b5aa0f5de1972ac24d7a4b1e346e46
Danny Loor <danny_loor00@hotmail.com>
2022-08-07 23:49:53 -0500

    some stuff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostsComponent } from './posts/posts.component';
+import { PetProfileComponent } from './pet-profile/pet-profile.component';
+import { OwnerProfileComponent } from './owner-profile/owner-profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'posts', component: PostsComponent },
+  { path: 'pet-profile', component: PetProfileComponent },
+  { path: 'owner-profile', component: OwnerProfileComponent },
+  { path: '**', redirectTo: 'posts' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PostsComponent } from './posts/posts.component';
+import { PetProfileComponent } from './pet-profile/pet-profile.component';
+import { OwnerProfileComponent } from './owner-profile/owner-profile.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    PetProfileComponent,
+    OwnerProfileComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
